36d40bdb51f6129850d6bb5986dff08b85fa5cc7
Abel-D <70660536+Abel-D@users.noreply.github.com>
2022-02-06 18:50:53 +0300

    fixes

---
+++ check.py
@@ -0,0 +1,62 @@
+def check(characters):
+        if (characters == 'ሐ') or (characters== 'ኀ') or (characters== 'ሃ') or (characters== 'ሓ') or (characters== 'ኃ'):
+              print(characters)  
+              rep='ሀ'
+              return rep
+        if (characters == 'ሑ') or (characters== 'ኁ') :
+              print(characters)
+              rep='ሁ'
+              return rep
+        if (characters == 'ሒ') or (characters== 'ኂ') :
+              print(characters)
+              rep = 'ሂ'
+              return rep
+
+        if (characters == 'ሔ') or (characters== 'ኄ') :
+              print(characters)
+              rep='ሄ'
+              return rep
+                
+        if (characters == 'ሕ') or (characters== 'ኅ') :
+              print(characters)
+              rep='ህ'
+              return rep
+            
+        if (characters == 'ሖ') or (characters== 'ኆ') :
+              print(characters)
+              rep='ሆ'
+              return rep
+
+        if (characters == 'ሰ'):
+              print(characters)
+              rep='ሠ'
+              return rep
+          
+        if (characters == 'ሱ'):
+              print(characters)
+              rep='ሡ'
+             
+        if (characters == 'ሲ'):
+              print(characters)
+              rep='ሢ'
+            
+        if (characters == 'ሳ'):
+              print(characters)
+              rep='ሣ'
+            
+        if (characters == 'ሴ'):
+              print(characters)
+              rep='ሤ'
+              return rep
+
+        if (characters == 'ስ'):
+              print(characters)
+              rep='ሥ'
+              return rep
+
+        if (characters == 'ሶ'):
+              print(characters)
+              rep='ሦ'
+              return rep
+        else:
+            return characters
--- read.py
+++ read.py
@@ -1,5 +1,5 @@
 import CompleteAlphabet
-
+import check
 import pyttsx3
 
 def computerSay():
@@ -21,59 +21,7 @@ def readFromCorpus(characters):
         import pygame
         pygame.mixer.pre_init(44100,16,2,4096)
         pygame.mixer.init()
-        rep =characters
-        if (characters == 'ሐ') or (characters== 'ኀ') or (characters== 'ሃ') or (characters== 'ሓ') or (characters== 'ኃ'):
-              print(characters)  
-              rep='ሀ'
-            
-        if (characters == 'ሑ') or (characters== 'ኁ') :
-              print(characters)
-              rep='ሁ'
-
-        if (characters == 'ሒ') or (characters== 'ኂ') :
-              print(characters)
-              rep = 'ሂ' 
-
-        if (characters == 'ሔ') or (characters== 'ኄ') :
-              print(characters)
-              rep='ሄ'
-                
-        if (characters == 'ሕ') or (characters== 'ኅ') :
-              print(characters)
-              rep='ህ'
-            
-        if (characters == 'ሖ') or (characters== 'ኆ') :
-              print(characters)
-              rep='ሆ'
-
-        if (characters == 'ሰ'):
-              print(characters)
-              rep='ሠ'
-          
-        if (characters == 'ሱ'):
-              print(characters)
-              rep='ሡ'
-             
-        if (characters == 'ሲ'):
-              print(characters)
-              rep='ሢ'
-            
-        if (characters == 'ሳ'):
-              print(characters)
-              rep='ሣ'
-            
-        if (characters == 'ሴ'):
-              print(characters)
-              rep='ሤ'
-
-        if (characters == 'ስ'):
-              print(characters)
-              rep='ሥ'
-
-        if (characters == 'ሶ'):
-              print(characters)
-              rep='ሦ'      
-            
+        rep =check.check(characters)              
         file='sound corpus/'+rep+'.wav'
         pygame.mixer.music.load(file)
         pygame.mixer.music.play()
